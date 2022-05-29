c0dc649cf59b72435baf3dd288241adf691316af
Frank Ketelaars <fketelaars@nl.ibm.com>
2022-05-29 15:28:06 +0200

    Gatsby test

--- doc/gatsby-config.js
+++ doc/gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description: 'Automated deployment of OpenShift and Cloud Paks',
     keywords: 'gatsby,theme,carbon',
   },
-  pathPrefix: "/CloudPakDeployer/cloud-pak-deployer",
+  pathPrefix: "",
   plugins: [
     {
       resolve: 'gatsby-plugin-manifest',
@@ -24,7 +24,7 @@ module.exports = {
         mediumAccount: 'carbondesign',
         repository: {
           baseUrl:
-            'https://github.ibm.com/CloudPakDeployer/cloud-pak-deployer',
+            'https://github.com/fketelaars/pages-test',
           subDirectory: '',
           branch: 'main',
         },
