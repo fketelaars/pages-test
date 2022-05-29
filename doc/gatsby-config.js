fe342519fc26e4689b19d6dd20112f850ef1a4be
Frank Ketelaars <fketelaars@nl.ibm.com>
2022-05-29 16:59:17 +0200

    Add start_url

--- doc/gatsby-config.js
+++ doc/gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       options: {
         name: 'Carbon Design Gatsby Theme',
         short_name: 'Gatsby Theme Carbon',
-        start_url: '/',
+        start_url: '/pages-test',
         background_color: '#ffffff',
         theme_color: '#0062ff',
         display: 'browser',
